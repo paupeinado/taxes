59df233fc4028c61c7c30d9d13e0bce396782707
Pau Peinado <paupeinado@gmail.com>
2019-02-16 09:31:14 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,84 @@
+new Vue({
+    el: '#pvp',
+    data: {
+        iva: 1.045,
+        pvl: null,
+        margen: null
+    },
+    computed: {
+        pvp_calc: function() {
+            if (this.pvl && this.margen) {
+                return ((this.pvl * this.iva) / ((100 - this.margen) / 100)).toFixed(2);
+            } else {
+                return '???';
+            }
+        },
+        pvp: function () {
+            return this.pvp_calc !== '???' ?  this.pvp_calc + '%' : this.pvp_calc;
+        },
+        ganancia: function () {
+            if (this.pvp_calc !== '???') {
+                return (this.pvp_calc - (this.pvl * this.iva)).toFixed(2) + ' €';
+            } else {
+                return '???';
+            }
+        }
+    }
+});
+
+new Vue({
+    el: '#margen',
+    data: {
+        iva: 1.045,
+        pvl: null,
+        dto: null,
+        pvp: null
+    },
+    computed: {
+        pvl_final: function() {
+            if (this.pvl && this.dto) {
+                return (this.pvl - (this.pvl * (this.dto / 100))).toFixed(2);
+            } else {
+                return '???';
+            }
+        },
+        margen: function () {
+            if (this.pvl_final !== '???' && this.pvp) {
+                return ((1  - ((this.pvl_final * this.iva) / this.pvp)) * 100).toFixed(2) + '%';
+            } else {
+                return '???';
+            }
+        },
+        ganancia: function () {
+            if (this.pvp && this.pvl_final) {
+                return (this.pvp - (this.pvl_final * this.iva)).toFixed(2) + '€';
+            } else {
+                return '???';
+            }
+        }
+    }
+});
+
+new Vue({
+    el: '#bonus',
+    data: {
+        uds: null,
+        uds_bonus: null
+    },
+    computed: {
+        uds_total: function() {
+            if (this.uds && this.uds_bonus) {
+                return parseInt(this.uds) + parseInt(this.uds_bonus);
+            } else {
+                return '???';
+            }
+        },
+        dto: function () {
+            if (this.uds_total !== '???') {
+                return ((this.uds_bonus / this.uds_total) * 100).toFixed(2) + '%';
+            } else {
+                return '???';
+            }
+        }
+    }
+});
